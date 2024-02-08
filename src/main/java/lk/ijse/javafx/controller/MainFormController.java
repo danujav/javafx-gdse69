a33c10eeaec06df2ba287a1a0652b10104bc7d9f
danu <danujagreru@gmail.com>
2024-02-08 16:38:15 +0530

    navigation done

--- src/main/java/lk/ijse/javafx/controller/MainFormController.java
+++ src/main/java/lk/ijse/javafx/controller/MainFormController.java
@@ -6,15 +6,33 @@ package lk.ijse.javafx.controller;
 */
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainFormController {
+    public AnchorPane rootNode;
+
     public void btnCustomerOnAction(ActionEvent actionEvent) {
     }
 
     public void btnItemOnAction(ActionEvent actionEvent) {
     }
 
-    public void btnBackOnAction(ActionEvent actionEvent) {
-        System.out.println("Back");
+    public void btnBackOnAction(ActionEvent actionEvent) throws IOException {
+        Parent rootNode = FXMLLoader.load(this.getClass().getResource("/view/login_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = (Stage) this.rootNode.getScene().getWindow();
+        stage.setScene(scene);
+
+
+        stage.centerOnScreen();
+        stage.setTitle("Login Form");
     }
 }
