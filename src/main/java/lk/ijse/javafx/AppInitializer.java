51b6758192f1e14c1c5d6d6e8279aa24fb8afa03
danu <danujagreru@gmail.com>
2024-02-08 10:12:02 +0530

    starting main window success

--- src/main/java/lk/ijse/javafx/AppInitializer.java
+++ src/main/java/lk/ijse/javafx/AppInitializer.java
@@ -5,8 +5,25 @@ package lk.ijse.javafx;
     @created 2/8/24 - 9:04 AM   
 */
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/sample_form.fxml"));
+
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.setTitle("JavaFX Application");
+
+        stage.show();
     }
 }
---
+++ src/main/java/lk/ijse/javafx/AppInitializerWrapper.java
@@ -0,0 +1,12 @@
+package lk.ijse.javafx;
+
+/*
+    @author DanujaV
+    @created 2/8/24 - 10:07 AM   
+*/
+
+public class AppInitializerWrapper {
+    public static void main(String[] args) {
+        AppInitializer.main(args);
+    }
+}
