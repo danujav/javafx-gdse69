515a2c0211ac5ffa96aaea2b00701cee675216b6
danu <danujagreru@gmail.com>
2024-02-08 09:44:17 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/javafx/AppInitializer.java
@@ -0,0 +1,12 @@
+package lk.ijse.javafx;
+
+/*
+    @author DanujaV
+    @created 2/8/24 - 9:04 AM   
+*/
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/lk/ijse/javafx/controller/SampleFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.javafx.controller;
+
+/*
+    @author DanujaV
+    @created 2/8/24 - 9:41 AM   
+*/
+
+public class SampleFormController {
+}
