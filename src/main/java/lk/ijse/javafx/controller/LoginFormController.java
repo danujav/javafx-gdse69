01e15b145e7a25b3db8fdcc8768082e910561adc
danu <danujagreru@gmail.com>
2024-02-08 11:50:33 +0530

    onAction for login button

--- src/main/java/lk/ijse/javafx/controller/LoginFormController.java
+++ src/main/java/lk/ijse/javafx/controller/LoginFormController.java
@@ -5,5 +5,18 @@ package lk.ijse.javafx.controller;
     @created 2/8/24 - 10:58 AM   
 */
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class LoginFormController {
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+        String userName = txtUserName.getText();
+        String pw = txtPassword.getText();
+
+        System.out.println(userName);
+        System.out.println(pw);
+    }
 }
